23a0379b4db5cde9b1c6c6e201d75c4be9e5b482
caioaugustoo <caioamfr@gmail.com>
2020-11-05 17:33:00 -0300

    Adicionada rota de detalhes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import OrphaneteMap from "./pages/OrphaneteMap";
+import OrphaneteDetails from "./pages/OrphaneteDetails";
 
 const { Navigator, Screen } = createStackNavigator();
 export default function Routes() {
@@ -10,6 +11,7 @@ export default function Routes() {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="OrphaneteMap" component={OrphaneteMap} />
+        <Screen name="OrphaneteDetails" component={OrphaneteDetails} />
       </Navigator>
     </NavigationContainer>
   );
