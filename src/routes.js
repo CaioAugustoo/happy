d89626dbfe89feaa4147f464c27237c198a430a0
caioaugustoo <caioamfr@gmail.com>
2020-11-05 17:31:00 -0300

    Correção na escrita da rota

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import OrphanatesMap from "./pages/OrphantesMap";
+import OrphaneteMap from "./pages/OrphaneteMap";
 
 const { Navigator, Screen } = createStackNavigator();
 export default function Routes() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="OrphanatesMap" component={OrphanatesMap} />
+        <Screen name="OrphaneteMap" component={OrphaneteMap} />
       </Navigator>
     </NavigationContainer>
   );
